7882d61c7dea75ad8c3e218388de29c03ed4a17f
Ismail Abdul Hakim <ismakim22@gmail.com>
2024-03-18 11:23:08 +0700

    v.1

---
+++ calculator.js
@@ -0,0 +1,17 @@
+function calculate() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+    var operator = document.getElementById('operator').value;
+    var result;
+
+    switch (operator) {
+        case 'add':
+            result = num1 + num2;
+            break;
+
+        default:
+            result = "Invalid operator";
+    }
+
+    document.getElementById('result').innerText = "Hasil : " + result;
+}
