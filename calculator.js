5e4372a81a5b55360334c3b647c055ac90d3cbf2
Ismail Abdul Hakim <ismakim22@gmail.com>
2024-03-18 11:25:12 +0700

    v.3

--- calculator.js
+++ calculator.js
@@ -11,7 +11,9 @@ function calculate() {
         case 'subtract':
             result = num1 - num2;
             break;
-
+        case 'multiply':
+            result = num1 * num2;
+            break;
         default:
             result = "Invalid operator";
     }
